df375a151789907d381e78a9ef68524abd9a9159
cutajarj2 <james.cutajar@blip.pt>
2018-09-18 16:03:08 +0100

    Added solution for max profit

---
+++ solutions/maximumsliceproblem/max_profit.py
@@ -0,0 +1,15 @@
+# This is the solution for Maximum Slice Problem > Max Profit
+#
+# This is marked as PAINLESS difficulty
+
+def solution(A):
+    global_max_sum = 0
+    local_max_sum = 0
+    for i in range(1, len(A)):
+        d = A[i] - A[i - 1]
+        local_max_sum = max(d, local_max_sum + d)
+        global_max_sum = max(local_max_sum, global_max_sum)
+    return global_max_sum
+
+print(solution([23171, 21011, 21123, 21366, 21013, 21367]))
+
