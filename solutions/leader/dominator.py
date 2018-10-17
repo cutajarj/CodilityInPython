36abb62f4ccbb1cb8179e2bcd4d962f7fc20963b
cutajarj2 <james.cutajar@blip.pt>
2018-10-17 16:03:19 +0100

    Added solution for leader

---
+++ solutions/leader/dominator.py
@@ -0,0 +1,23 @@
+# This is the solution for Leader > Dominator
+#
+# This is marked as PAINLESS difficulty
+
+def solution(A):
+    consecutive_size = 0
+    candidate = 0
+    for item in A:
+        if consecutive_size == 0:
+            candidate = item
+            consecutive_size += 1
+        elif candidate == item:
+            consecutive_size += 1
+        else:
+            consecutive_size -= 1
+    occurance = A.count(candidate)
+    if occurance > (len(A)/2):
+        return A.index(candidate)
+    else:
+        return -1
+
+
+print(solution([3,0,1,1,4,1,1]))
