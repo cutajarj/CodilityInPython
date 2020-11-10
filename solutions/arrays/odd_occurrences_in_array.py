dba8361135af95d31110ead7b56a63a11931aba5
cutajarj <james.cutajar@gmail.com>
2020-11-10 19:37:36 +0000

    Solution to odd occurrences in array

---
+++ solutions/arrays/odd_occurrences_in_array.py
@@ -0,0 +1,12 @@
+def solution(A):
+    numbers = set([])
+    for n in A:
+        if n in numbers:
+            numbers.discard(n)
+        else:
+            numbers.add(n)
+    return numbers.pop()
+
+
+print(solution([9, 3, 9, 3, 9, 7, 9]))
+
